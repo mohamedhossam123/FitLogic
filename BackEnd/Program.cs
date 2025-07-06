4d91272860973b63c6177bd32e6d77d211ea9acf
Mohamed_Hossam <mohamedhossam25709@gmail.com>
2025-07-06 15:51:51 +0300

    Some proggram.cs

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,13 +1,16 @@
+
+
 using BackEnd.Models;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using MyApiProject.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 builder.Services.AddControllers();
 builder.Services.AddScoped<ICaloriesService, CaloriesService>();
 builder.Services.AddScoped<IWorkoutService, WorkoutService>();
@@ -17,14 +20,17 @@ builder.Services.AddDbContext<FitLogicContext>(options =>
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigin", builder =>
-        builder.WithOrigins("feisty-nature-production.up.railway.app")
-               .AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowCredentials());
+    options.AddPolicy("AllowSpecificOrigin", policyBuilder =>
+        policyBuilder.WithOrigins(
+            "https://feisty-nature-production.up.railway.app",
+            "http://localhost:3000"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials());
 });
 
 
